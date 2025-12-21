7a4053c00247c134f210b5c564c1771ed541b8ea
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2025-12-21 22:14:55 +0100

    Added Handlers

--- PositiveNewsPlatform.Api/Program.cs
+++ PositiveNewsPlatform.Api/Program.cs
@@ -1,3 +1,7 @@
+using PositiveNewsPlatform.Application.Articles.Create;
+using PositiveNewsPlatform.Application.Articles.GetById;
+using PositiveNewsPlatform.Application.Articles.GetLatest;
+using PositiveNewsPlatform.Application.Articles.Update;
 using PositiveNewsPlatform.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +10,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Infrastructure (SQL + Mongo + Redis)
 builder.Services.AddInfrastructure(builder.Configuration);
+
+// Handlers (Application)
+builder.Services.AddScoped<CreateArticleHandler>();
+builder.Services.AddScoped<UpdateArticleHandler>();
+builder.Services.AddScoped<GetArticleByIdHandler>();
+builder.Services.AddScoped<GetLatestArticlesHandler>();
 
 var app = builder.Build();
 
